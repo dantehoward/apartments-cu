ef9ab41bb143267e3500463e16d5c3698651092d
Dante <34189866+dantehoward@users.noreply.github.com>
2023-02-24 21:19:01 -0600

    Restructed files and directories

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+             <h1>App</h1>
+        );
+    }
+}
+
+export default App;
